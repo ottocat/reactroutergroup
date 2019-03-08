b0f3db8c0efee718c06854f2c697bf77195f5bea
Sandra <sandra.menendez003@mymdc.net>
2019-03-08 23:30:49 +0000

    pulling changes

--- src/js/views/Blog.jsx
+++ src/js/views/Blog.jsx
@@ -8,44 +8,22 @@ import "../../styles/demo.css";
 export class Blog extends React.Component {
 	render() {
 		return (
-			<div className="container">
-				<ul className="list-group">
-					<Context.Consumer>
-						{({ store, actions }) => {
-							return store.demo.map((item, index) => {
-								return (
-									<li
-										key={index}
-										className="list-group-item d-flex justify-content-between"
-										style={{ background: item.background }}>
-										<Link to={"/single/" + index}>
-											<span>Link to: {item.title}</span>
-										</Link>
-										<p style={{ color: item.initial }}>
-											{
-												"Check store/store.js scroll to the actions to see the code "
-											}
-										</p>
-										<button
-											className="btn btn-success"
-											onClick={() =>
-												actions.changeColor(
-													index,
-													"orange"
-												)
-											}>
-											Change Color
-										</button>
-									</li>
-								);
-							});
-						}}
-					</Context.Consumer>
-				</ul>
-				<br />
-				<Link to="/">
-					<button className="btn btn-primary">Back home</button>
-				</Link>
+			<div className="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+				<div className="col-md-6 px-0">
+					<h1 className="display-4 font-italic">
+						Title of a longer featured blog post
+					</h1>
+					<p className="lead my-3">
+						Multiple lines of text that form the lede, informing new
+						readers quickly and efficiently about what's most
+						interesting in this post's contents.
+					</p>
+					<p className="lead mb-0">
+						<a href="#" class="text-white font-weight-bold">
+							Continue reading...
+						</a>
+					</p>
+				</div>
 			</div>
 		);
 	}
