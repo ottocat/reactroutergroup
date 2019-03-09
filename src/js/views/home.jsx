6e62ddd944f6b6f36743fe4d7085a785b5169846
Andrew J <andrewsjorquera@gmail.com>
2019-03-09 00:11:55 +0000

    card link functionality

--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -14,79 +14,101 @@ export class Home extends React.Component {
 		return (
 			<div className="row mx-auto">
 				{/* First Column */}
+
 				<div className="col-6 firstCard">
-					<div className="card bg-dark">
-						<img src={danceBlog} className="card-img" alt="..." />
-						<div className="card-img-overlay d-flex flex-column">
-							<h4 className="card-title text-white mt-2 font-weight-bold">
-								Featured Blog
-							</h4>
-							<p className="card-text text-white mt-auto font-weight-bold">
-								{
-									"Following an introvert's journey through the Salsa Community"
-								}
-							</p>
+					<Link to="/Blog">
+						<div className="card bg-dark">
+							<img
+								src={danceBlog}
+								className="card-img"
+								alt="..."
+							/>
+							<div className="card-img-overlay d-flex flex-column">
+								<h4 className="card-title text-white mt-2 font-weight-bold">
+									Featured Blog
+								</h4>
+								<p className="card-text text-white mt-auto font-weight-bold">
+									{
+										"Following an introvert's journey through the Salsa Community"
+									}
+								</p>
+							</div>
 						</div>
-					</div>
+					</Link>
 				</div>
+
 				<div className="col-3 secondCol">
 					{" "}
 					{/* Second Column first Card */}
-					<div className="card bg-dark">
-						<img src={diMaria} className="card-img" alt="..." />
-						<div className="card-img-overlay d-flex flex-column">
-							<h4 className="card-title text-danger bg-dark mt-2 font-weight-bold">
-								Fulltime
-							</h4>
-							<p className="card-text text-danger bg-dark mt-auto font-weight-bold">
-								{
-									"A blog proving hindsight really is 20/20, nitpickingevery big game"
-								}
-							</p>
+					<Link to="/Blog">
+						<div className="card bg-dark">
+							<img src={diMaria} className="card-img" alt="..." />
+							<div className="card-img-overlay d-flex flex-column">
+								<h4 className="card-title text-danger bg-dark mt-2 font-weight-bold">
+									Fulltime
+								</h4>
+								<p className="card-text text-danger bg-dark mt-auto font-weight-bold">
+									{
+										"A blog proving hindsight really is 20/20, nitpickingevery big game"
+									}
+								</p>
+							</div>
 						</div>
-					</div>
+					</Link>
 					{/* Second Column Second Card */}
 					<div className="card bg-dark mt-3">
-						<img src={GreatWall} className="card-img" alt="..." />
-						<div className="card-img-overlay d-flex flex-column">
-							<h4 className="card-title mt-2 font-weight-bold traveltitle">
-								Viajando
-							</h4>
-							<p className="card-text mt-auto font-weight-bold travel">
-								{
-									"Seeing the world, and still finding the time to code"
-								}
-							</p>
-						</div>
+						<Link to="/Blog">
+							<img
+								src={GreatWall}
+								className="card-img"
+								alt="..."
+							/>
+							<div className="card-img-overlay d-flex flex-column">
+								<h4 className="card-title mt-2 font-weight-bold traveltitle">
+									Viajando
+								</h4>
+								<p className="card-text mt-auto font-weight-bold travel">
+									{
+										"Seeing the world, and still finding the time to code"
+									}
+								</p>
+							</div>
+						</Link>
 					</div>
 				</div>
 				{/* Third Column first Card */}
 				<div className="col-3 thirdCol">
 					{" "}
-					<div className="card bg-dark">
-						<img src={Cooks} className="card-img" alt="..." />
-						<div className="card-img-overlay d-flex flex-column">
-							<h4 className="card-title mt-2 font-weight-bold cooktitle">
-								Food
-							</h4>
-							<p className="card-text mt-auto  font-weight-bold cooktext">
-								{"Cooking while coding: a comprehensive guide."}
-							</p>
+					<Link to="/Blog">
+						<div className="card bg-dark">
+							<img src={Cooks} className="card-img" alt="..." />
+							<div className="card-img-overlay d-flex flex-column">
+								<h4 className="card-title mt-2 font-weight-bold cooktitle">
+									Food
+								</h4>
+								<p className="card-text mt-auto  font-weight-bold cooktext">
+									{
+										"Cooking while coding: a comprehensive guide."
+									}
+								</p>
+							</div>
 						</div>
-					</div>
+					</Link>
 					{/* Third Column Second Card */}
 					<div className="card bg-dark mt-3">
-						<img src={Boris} className="card-img" alt="..." />
-						<div className="card-img-overlay d-flex flex-column">
-							<h4 className="card-title text-danger bg-dark  mt-2 font-weight-bold">
-								{"Boris' retro coding"}
-							</h4>
-							<p className="card-text text-danger bg-dark mt-auto font-weight-bold boris">
-								{
-									"A look through former bond villain and elite hacker, Boris' eyes into the world of tech from the 90s "
-								}
-							</p>
-						</div>
+						<Link to="/Blog">
+							<img src={Boris} className="card-img" alt="..." />
+							<div className="card-img-overlay d-flex flex-column">
+								<h4 className="card-title text-danger bg-dark  mt-2 font-weight-bold">
+									{"Boris' retro coding"}
+								</h4>
+								<p className="card-text text-danger bg-dark mt-auto font-weight-bold boris">
+									{
+										"A look through former bond villain and elite hacker, Boris' eyes into the world of tech from the 90s "
+									}
+								</p>
+							</div>
+						</Link>
 					</div>
 				</div>
 			</div>
