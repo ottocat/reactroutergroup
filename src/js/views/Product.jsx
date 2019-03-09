d83b09d141e8c1662e34e297c05f1ac7242a85e7
Marcelo <marceloindart@gmail.com>
2019-03-09 21:02:05 +0000

    Product

--- src/js/views/Product.jsx
+++ src/js/views/Product.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "../../styles/Product.css";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext.jsx";
-
-let commiy = 0;
 
 export class Product extends React.Component {
 	render() {
@@ -13,22 +10,22 @@ export class Product extends React.Component {
 				<div className="card">
 					<div className="container-fliud">
 						<div className="wrapper row">
-							<div className="preview col-md-6">
+							<div className="preview col-6">
 								<div className="preview-pic tab-content">
 									<div className="tab-pane active" id="pic-1">
-										<img src="http://placekitten.com/400/252" />
+										<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSq5T4Zo0VR0_z8FtW7Blit5ZCig1kza79O2OB5FMkzuc07b_wCtQ" />
 									</div>
 									<div className="tab-pane" id="pic-2">
-										<img src="http://placekitten.com/400/252" />
+										<img src="https://target.scene7.com/is/image/Target/GUEST_c940683e-4253-445b-838a-11210e36e5f6?wid=488&hei=488&fmt=pjpeg" />
 									</div>
 									<div className="tab-pane" id="pic-3">
-										<img src="http://placekitten.com/400/252" />
+										<img src="https://sc01.alicdn.com/kf/HTB1AJXMKVXXXXb0XpXXq6xXFXXXI/most-soft-irish-dance-shoes-men-genuine.jpg_350x350.jpg" />
 									</div>
 									<div className="tab-pane" id="pic-4">
-										<img src="http://placekitten.com/400/252" />
+										<img src="https://i.pinimg.com/236x/6a/dd/1d/6add1d941228c7650c3c8ea3adbf1c0d--ballroom-dance-shoes-salsa-shoes.jpg" />
 									</div>
 									<div className="tab-pane" id="pic-5">
-										<img src="http://placekitten.com/400/252" />
+										<img src="https://images-na.ssl-images-amazon.com/images/I/614QjaLuVJL._UY395_.jpg" />
 									</div>
 								</div>
 								<ul className="preview-thumbnail nav nav-tabs">
@@ -68,6 +65,85 @@ export class Product extends React.Component {
 										</a>
 									</li>
 								</ul>
+							</div>
+							<div className="details col-6">
+								<h3 className="product-title">
+									men&apos;s shoes fashion
+								</h3>
+								<div className="rating">
+									<div className="stars">
+										<span className="fa fa-star checked" />
+										<span className="fa fa-star checked" />
+										<span className="fa fa-star checked" />
+										<span className="fa fa-star" />
+										<span className="fa fa-star" />
+									</div>
+									<span className="review-no">
+										41 reviews
+									</span>
+								</div>
+								<p className="product-description">
+									Suspendisse quos? Tempus cras iure
+									temporibus? Eu laudantium cubilia sem sem!
+									Repudiandae et! Massa senectus enim minim
+									sociosqu delectus posuere.
+								</p>
+								<h4 className="price">
+									current price: <span>$180</span>
+								</h4>
+								<p className="vote">
+									<strong>91%</strong> of buyers enjoyed this
+									product! <strong>(87 votes)</strong>
+								</p>
+								<h5 className="sizes">
+									sizes:
+									<span
+										className="size"
+										data-toggle="tooltip"
+										title="small">
+										s
+									</span>
+									<span
+										className="size"
+										data-toggle="tooltip"
+										title="medium">
+										m
+									</span>
+									<span
+										className="size"
+										data-toggle="tooltip"
+										title="large">
+										l
+									</span>
+									<span
+										className="size"
+										data-toggle="tooltip"
+										title="xtra large">
+										xl
+									</span>
+								</h5>
+								<h5 className="colors">
+									colors:
+									<span
+										className="color orange not-available"
+										data-toggle="tooltip"
+										title="Not In store"
+									/>
+									<span className="color green" />
+									<span className="color blue" />
+								</h5>
+								<div className="action">
+									<button
+										className="add-to-cart btn btn-default"
+										type="button">
+										add to cart
+									</button>
+									<button
+										className="like btn btn-default"
+										type="button">
+										<span className="fa fa-heart" />
+									</button>
+								</div>
 							</div>
 						</div>
 					</div>
