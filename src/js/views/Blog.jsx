075b5daeb82451f56cd14f57b071e451e6b6deef
Andrew J <andrewsjorquera@gmail.com>
2019-03-11 00:51:03 +0000

    potential changes

--- src/js/views/Blog.jsx
+++ src/js/views/Blog.jsx
@@ -15,7 +15,7 @@ export class Blog extends React.Component {
 					</h1>
 					<p className="lead my-3">
 						{
-							"Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's mostinteresting in this post's contents"
+							"Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents"
 						}
 					</p>
 					<p className="lead mb-0">
