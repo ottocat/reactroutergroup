7d44b1faea6fd445001e66b548c5407c363f339a
Andrew J <andrewsjorquera@gmail.com>
2019-03-12 04:03:30 +0000

    added context functionality to home page, open for use from blog page

--- src/js/store/store.js
+++ src/js/store/store.js
@@ -1,3 +1,8 @@
+// import Dance from "https://www.hubbardstreetdance.com/media/1149/hsd160707_0409-isolated-dancer-only.jpg?anchor=center&mode=crop&width=1140&height=600&rnd=131617969130000000";
+import diMaria from "../../img/DIMARIA.jpg";
+import GreatWall from "../../img/greatWall.jpg";
+import Boris from "../../img/Coding.jpg";
+import Cooks from "../../img/Cooking.jpg";
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
@@ -11,6 +16,46 @@ const getState = ({ getStore, setStore }) => {
 					title: "SECOND",
 					background: "white",
 					initial: "white"
+				}
+			],
+			blogs: [
+				{
+					title: "Featured Blog",
+					description: "More stuff",
+					date: "01/01/0001"
+				}
+			],
+			blogs2: [
+				{
+					title: "Extra Time",
+					description:
+						"A blog proving hindsight really is 20/20, nitpickingevery big game",
+					image: diMaria,
+					date: "1/24/19"
+				},
+				{
+					title: "Card number 2",
+					description:
+						"Seeing the world, and still finding the time to code",
+					image: GreatWall,
+					date: "01/10/10",
+					style: " mt-3"
+				}
+			],
+			blogs3: [
+				{
+					title: "Cooking Blog",
+					description: "Cooking while coding: a comprehensive guide.",
+					image: Cooks,
+					date: "01/10/10"
+				},
+				{
+					title: "Boris' Bootcamp",
+					description:
+						"A look through former bond villain and elite hacker, Boris' eyes into the world of tech from the 90s",
+					image: Boris,
+					date: "01/10/10",
+					style: " mt-3"
 				}
 			]
 		},
--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PropType from "prop-types";
+import DemoList from "../component/demoList.jsx";
+import ThirdColumn from "../component/3rdcolumn.jsx";
 
 import "../../styles/home.css";
 import danceBlog from "../../img/Michelle and I hurricane social.jpeg";
@@ -8,96 +10,43 @@ import diMaria from "../../img/DIMARIA.jpg";
 import GreatWall from "../../img/greatWall.jpg";
 import Boris from "../../img/Coding.jpg";
 import Cooks from "../../img/Cooking.jpg";
+import { Context } from "../store/appContext.jsx";
 
 export class Home extends React.Component {
 	render() {
 		return (
 			<div className="row mx-auto allCards">
 				{/* First Column */}
-
-				<div className="col-6 firstCard">
-					<Link to="/Blog">
-						<div className="card  ">
-							<img
-								src={danceBlog}
-								className="card-img"
-								alt="..."
-							/>
-							<div className="card-img-overlay d-flex flex-column">
-								<h4 className="card-title text-white mt-2 font-weight-bold firstCardTitle">
-									Featured Blog
-								</h4>
-								<p className="card-text text-white mt-auto font-weight-bold firstCardText">
-									{
-										"Following an introvert's journey through the Salsa Community"
-									}
-								</p>
+				<Context.Consumer>
+					{({ store, actions }) => {
+						return (
+							<div className="col-6 firstCard">
+								<Link to="/Blog">
+									<div className="card  ">
+										<img
+											src={danceBlog}
+											className="card-img"
+											alt="..."
+										/>
+										<div className="card-img-overlay d-flex flex-column">
+											<h4 className="card-title text-white mt-2 font-weight-bold firstCardTitle">
+												{store.blogs[0].title}
+											</h4>
+											<p className="card-text text-white mt-auto font-weight-bold firstCardText">
+												{store.blogs[0].description}
+											</p>
+										</div>
+									</div>
+								</Link>
 							</div>
-						</div>
-					</Link>
-				</div>
-
+						);
+					}}
+				</Context.Consumer>
 				<div className="col-3 secondCol">
-					{" "}
-					{/* Second Column first Card */}
-					<Link to="/Blog">
-						<div className="card">
-							<img src={diMaria} className="card-img" alt="..." />
-							<div className="card-img-overlay d-flex flex-column">
-								<p className="card-text  text-white   mt-auto font-weight-bold rounded secondCardText">
-									{
-										"A blog proving hindsight really is 20/20, nitpickingevery big game"
-									}
-								</p>
-							</div>
-						</div>
-					</Link>
-					{/* Second Column Second Card */}
-					<div className="card   mt-3">
-						<Link to="/Blog">
-							<img
-								src={GreatWall}
-								className="card-img"
-								alt="..."
-							/>
-							<div className="card-img-overlay d-flex flex-column">
-								<p className="card-text text-white mt-auto font-weight-bold thirdCardText">
-									{
-										"Seeing the world, and still finding the time to code"
-									}
-								</p>
-							</div>
-						</Link>
-					</div>
+					<DemoList />
 				</div>
-				{/* Third Column first Card */}
 				<div className="col-3 thirdCol">
-					{" "}
-					<Link to="/Blog">
-						<div className="card  ">
-							<img src={Cooks} className="card-img" alt="..." />
-							<div className="card-img-overlay d-flex flex-column">
-								<p className="card-text mt-auto text-white font-weight-bold fourthCardText">
-									{
-										"Cooking while coding: a comprehensive guide."
-									}
-								</p>
-							</div>
-						</div>
-					</Link>
-					{/* Third Column Second Card */}
-					<div className="card   mt-3">
-						<Link to="/Blog">
-							<img src={Boris} className="card-img" alt="..." />
-							<div className="card-img-overlay d-flex flex-column">
-								<p className="card-text  text-white mt-auto font-weight-bold fifthCardText">
-									{
-										"A look through former bond villain and elite hacker, Boris' eyes into the world of tech from the 90s "
-									}
-								</p>
-							</div>
-						</Link>
-					</div>
+					<ThirdColumn />
 				</div>
 			</div>
 		);
@@ -110,3 +59,66 @@ export class Home extends React.Component {
 // 	text1= "A blog proving hindsight really is 20/20, nitpicking every big game"
 // 	/>,
 // 	);
+
+// Hard coded cards <div className="col-3 secondCol">
+// 				{" "}
+// 				{/* Second Column first Card */}
+// 				<Link to="/Blog">
+// 					<div className="card">
+// 						<img src={diMaria} className="card-img" alt="..." />
+// 						<div className="card-img-overlay d-flex flex-column">
+// 							<p className="card-text  text-white   mt-auto font-weight-bold rounded secondCardText">
+// 								{
+// 									"A blog proving hindsight really is 20/20, nitpickingevery big game"
+// 								}
+// 							</p>
+// 						</div>
+// 					</div>
+// 				</Link>
+// 				{/* Second Column Second Card */}
+// 				<div className="card   mt-3">
+// 					<Link to="/Blog">
+// 						<img
+// 							src={GreatWall}
+// 							className="card-img"
+// 							alt="..."
+// 						/>
+// 						<div className="card-img-overlay d-flex flex-column">
+// 							<p className="card-text text-white mt-auto font-weight-bold thirdCardText">
+// 								{
+// 									"Seeing the world, and still finding the time to code"
+// 								}
+// 							</p>
+// 						</div>
+// 					</Link>
+// 				</div>
+// 			</div>
+// 			{/* Third Column first Card */}
+// 			<div className="col-3 thirdCol">
+// 				{" "}
+// 				<Link to="/Blog">
+// 					<div className="card  ">
+// 						<img src={Cooks} className="card-img" alt="..." />
+// 						<div className="card-img-overlay d-flex flex-column">
+// 							<p className="card-text mt-auto text-white font-weight-bold fourthCardText">
+// 								{
+// 									"Cooking while coding: a comprehensive guide."
+// 								}
+// 							</p>
+// 						</div>
+// 					</div>
+// 				</Link>
+// 				{/* Third Column Second Card */}
+// 				<div className="card   mt-3">
+// 					<Link to="/Blog">
+// 						<img src={Boris} className="card-img" alt="..." />
+// 						<div className="card-img-overlay d-flex flex-column">
+// 							<p className="card-text  text-white mt-auto font-weight-bold fifthCardText">
+// 								{
+// 									"A look through former bond villain and elite hacker, Boris' eyes into the world of tech from the 90s "
+// 								}
+// 							</p>
+// 						</div>
+// 					</Link>
+// 				</div>
+// 			</div>
